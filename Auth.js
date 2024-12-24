4f45a1afb589cfd1e99d5259e6f2d7765d108a0b
Meta-2024-maker <justbrownies3@gmail.com>
2024-12-24 18:53:50 +0000

    Create Auth.js

---
+++ Auth.js
@@ -0,0 +1,21 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAy_IoLTw4bT5dBLb0tdgQ4QGY9viDyySM",
+    authDomain: "doglovershub-2a036.firebaseapp.com",
+    projectId: "doglovershub-2a036",
+    storageBucket: "doglovershub-2a036.firebasestorage.app",
+    messagingSenderId: "1022201852544",
+    appId: "1:1022201852544:web:c2e042b79ceddfec3eec65",
+    measurementId: "G-1G7Q8EKZM5"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
