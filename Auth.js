613d934695481ffe0b2eab4ae9d31260c0a96ade
Meta-2024-maker <justbrownies3@gmail.com>
2025-01-22 20:34:10 +0100

    Update Auth.js

--- Auth.js
+++ Auth.js
@@ -1,31 +1,40 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAy_IoLTw4bT5dBLb0tdgQ4QGY9viDyySM",
-    authDomain: "doglovershub-2a036.firebaseapp.com",
-    projectId: "doglovershub-2a036",
-    storageBucket: "doglovershub-2a036.firebasestorage.app",
-    messagingSenderId: "1022201852544",
-    appId: "1:1022201852544:web:c2e042b79ceddfec3eec65",
-    measurementId: "G-1G7Q8EKZM5"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB4BOs3e3jQJX16568tz6YiV02aH-DJoUk",
+  authDomain: "doglovershub-f167d.firebaseapp.com",
+  projectId: "doglovershub-f167d",
+  storageBucket: "doglovershub-f167d.firebasestorage.app",
+  messagingSenderId: "573939341026",
+  appId: "1:573939341026:web:77a67d5221f19918b2f0bb",
+  measurementId: "G-6CH39FEYEL"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 //inputs
 const email = document.getElementById("email-input").value;
 const password = document.getElementById("password-input").value;
 
 //submit button
 const submit = document.getElementById("Login");
-submit.addEventListener("click",function(event){
+submit.addEventListener("click", function (event) {
   event.preventDefault()
-  alert(5)
-})
+
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      alert("Creating Account...")
+      window.location.href = "Grand.html"
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
